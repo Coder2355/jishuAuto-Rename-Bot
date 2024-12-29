4a4038e77c9e6503e4e9cc5a08ec3c7ab8b57b63
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-12-29 21:08:37 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -1,4 +1,5 @@
 import random
+import base64
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery, Message, InputMediaPhoto
 
